cfb64465efa9ef0669ecebc4532173e86d016ba5
ArulvinMoses <arulvin.g@capillarytech.com>
2025-05-02 09:57:30 +0530

    Changes

--- src/config/logger-config.js
+++ src/config/logger-config.js
@@ -1,20 +1,19 @@
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, label, printf } = format;
 
 const customFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${label}] ${level}: ${message}`;
-}
-);
+});
 const logger = createLogger({
   format: combine(
-    timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-    label({ label: 'Project-MS' }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    label({ label: "Project-MS" }),
     customFormat
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: 'combined.log' })
-  ]
+    new transports.File({ filename: "combined.log" }),
+  ],
 });
 
 module.exports = logger;
